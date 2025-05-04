fac7d775d7a26393c38a37404b88cecc537502c1
D4VC71 <82661768+D4VC71@users.noreply.github.com>
2025-05-04 02:28:42 +0200

    Add files via upload

---
+++ 1/Solution.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode_Solutions
+{
+    internal class Solution
+    {
+        public int[] TwoSum(int[] nums, int target)
+        {
+            int[] indices = { 0, 0 };
+
+            for (int i = 0; i < nums.Length - 1; i++)
+            {
+                for (int j = i + 1; j < nums.Length; j++)
+                {
+                    if (nums[i] + nums[j] == target)
+                    {
+                        indices[0] = i;
+                        indices[1] = j;
+                        break;
+                    }
+                }
+            }
+
+            return indices;
+        }
+    }
+}
