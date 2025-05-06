3b211783da00b638fe95cd2e3c11bd4619409e1d
D4VC71 <82661768+D4VC71@users.noreply.github.com>
2025-05-06 15:54:41 +0200

    Add files via upload

---
+++ 1920/Solution.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode_Solutions
+{
+    public class Solution
+    {
+        public int[] BuildArray(int[] nums)
+        {
+            int n = nums.Length;
+            int[] ans = new int[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                ans[i] = nums[nums[i]];
+            }
+
+            return ans;
+        }
+    }
+}
