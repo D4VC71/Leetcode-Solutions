34767f066d6e60e36195a102ff75cc38b1c6714a
D4VC71 <82661768+D4VC71@users.noreply.github.com>
2025-05-16 19:16:38 +0200

    Add files via upload

---
+++ 2176/Solution.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode_Solutions
+{
+    public class Solution
+    {
+        public int CountPairs(int[] nums, int k)
+        {
+            int pairs = 0;
+
+            for (int i = 0; i < nums.Length - 1; i++)
+            {
+                for (int j = i + 1; j < nums.Length; j++)
+                {
+                    if (nums[i] == nums[j] && ((i * j) % k == 0))
+                    {
+                        pairs++;
+                    }
+                }
+            }
+
+            return pairs;
+        }
+    }
+}
