36f9047855570508a040a25b8ece4150572369af
D4VC71 <82661768+D4VC71@users.noreply.github.com>
2025-05-16 18:58:24 +0200

    Add files via upload

---
+++ 1550/Solution.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode_Solutions
+{
+    public class Solution
+    {
+        public bool ThreeConsecutiveOdds(int[] arr)
+        {
+            int count, i;
+            count = i = 0;
+
+            while (i < arr.Length && count != 3)
+            {
+                if (arr[i] % 2 != 0)
+                {
+                    count++;
+                }
+                else
+                {
+                    count = 0;
+                }
+                i++;
+            }
+
+            if (count == 3)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
