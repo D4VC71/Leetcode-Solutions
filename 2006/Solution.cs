e9032b209a38960f3a12ea4ebb5eba3f74d5a637
D4VC71 <82661768+D4VC71@users.noreply.github.com>
2025-05-16 19:03:31 +0200

    Add files via upload

---
+++ 2006/Solution.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode_Solutions
+{
+    public class Solution
+    {
+        public int CountKDifference(int[] nums, int k)
+        {
+            int numberOfPairs = 0;
+
+            for (int i = 0; i < nums.Length - 1; i++)
+            {
+                for (int j = i + 1; j < nums.Length; j++)
+                {
+                    if (Math.Abs(nums[i] - nums[j]) == k)
+                    {
+                        numberOfPairs++;
+                    }
+                }
+            }
+
+            return numberOfPairs;
+        }
+    }
+}
